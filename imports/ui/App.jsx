8308d03876e83108cf3598f1d6b6652789feb6ac
sebastianff <sebastianffarago@gmail.com>
2016-08-17 18:17:51 +0200

    added basic formsy form for testing/customization

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,1 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import Formsy from 'formsy-react';
+
+const MyOwnInput = React.createClass({
+
+    // Add the Formsy Mixin
+    mixins: [Formsy.Mixin],
+
+    // setValue() will set the value of the component, which in
+    // turn will validate it and the rest of the form
+    changeValue(event) {
+      this.setValue(event.currentTarget.value);
+    },
+
+    render() {
+      // Set a specific className based on the validation
+      // state of this component. showRequired() is true
+      // when the value is empty and the required prop is
+      // passed to the input. showError() is true when the
+      // value typed is invalid
+      const className = this.showRequired() ? 'required' : this.showError() ? 'error' : null;
+
+      // An error message is returned ONLY if the component is invalid
+      // or the server has returned an error message
+      const errorMessage = this.getErrorMessage();
+
+      return (
+        <div className={className}>
+          <input type="text" onChange={this.changeValue} value={this.getValue()}/>
+          <span>{errorMessage}</span>
+        </div>
+      );
+    }
+  });
+  
+export default MyAppForm = React.createClass({
+    getInitialState() {
+      return {
+        canSubmit: false
+      }
+    },
+    enableButton() {
+      this.setState({
+        canSubmit: true
+      });
+    },
+    disableButton() {
+      this.setState({
+        canSubmit: false
+      });
+    },
+    submit(model) {
+      someDep.saveEmail(model.email);
+    },
+    render() {
+      return (
+        <Formsy.Form onValidSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+          <MyOwnInput name="email" validations="isEmail" validationError="This is not a valid email" required/>
+          <button type="submit" disabled={!this.state.canSubmit}>Submit</button>
+        </Formsy.Form>
+      );
+    }
+  });
